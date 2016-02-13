6e36732150295869a8ddaebbaf6ff07b76eb06b8
Sebastian Ramirez <tiangolo@gmail.com>
2016-02-13 21:22:27 +0100

    Add default Python example app

---
+++ uwsgi-nginx/app/main.py
@@ -0,0 +1,5 @@
+# test.py
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    #return [b"Hello World"] # python3
+    return ["Hello World"] # python2
