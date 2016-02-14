f9a16e6697f51fc9585a6966db413f28f4a8d2df
Sebastian Ramirez <tiangolo@gmail.com>
2016-02-14 10:07:32 +0100

    Rename base image

--- base/app/main.py
+++ base/app/main.py
@@ -2,4 +2,4 @@
 def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
     #return [b"Hello World"] # python3
-    return ["Hello World"] # python2
+    return ["Hello World from uwsgi-nginx:base"] # python2
