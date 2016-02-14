c327c66c175b5ff196305d58a9fd5bb1ca20046e
Sebastian Ramirez <tiangolo@gmail.com>
2016-02-14 11:17:22 +0100

    Fix pseudo typo

--- flask/app/main.py
+++ flask/app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     print "test"
-    return "Hello World from uwsgi-nginx:flask!"
+    return "Hello World from uwsgi-nginx:flask"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=80)
