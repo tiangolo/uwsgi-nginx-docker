44a450b90b8d7989a56cf58803c6405c2cbd731f
Starchy <starchy@eff.org>
2017-08-16 15:34:39 -0700

    adds python2.7-alpine3.4 tag

---
+++ python2.7-alpine3.4/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# Get the maximum upload file size for Nginx, default to 0: unlimited
+USE_NGINX_MAX_UPLOAD=${NGINX_MAX_UPLOAD:-0}
+# Generate Nginx config for maximum upload file size
+echo "client_max_body_size $USE_NGINX_MAX_UPLOAD;" > /etc/nginx/conf.d/upload.conf
+
+exec "$@"
