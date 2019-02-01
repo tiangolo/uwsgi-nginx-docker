625b7e844d4db1f6ddb148edee4714fa4f6014d3
Sebastián Ramírez <tiangolo@gmail.com>
2019-02-01 21:03:32 +0400

    :sparkles: Generate Nginx configs from entrypoint and tests

--- python3.7-alpine3.7/entrypoint.sh
+++ python3.7-alpine3.7/entrypoint.sh
@@ -1,35 +1,58 @@
 #!/usr/bin/env sh
 set -e
 
-# Get the maximum upload file size for Nginx, default to 0: unlimited
-USE_NGINX_MAX_UPLOAD=${NGINX_MAX_UPLOAD:-0}
-# Generate Nginx config for maximum upload file size
-echo "client_max_body_size $USE_NGINX_MAX_UPLOAD;" > /etc/nginx/conf.d/upload.conf
-
 # Explicitly add installed Python packages and uWSGI Python packages to PYTHONPATH
 # Otherwise uWSGI can't import Flask
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/site-packages:/usr/lib/python3.6/site-packages
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages:/usr/lib/python2.7/site-packages
+
+# Get the maximum upload file size for Nginx, default to 0: unlimited
+USE_NGINX_MAX_UPLOAD=${NGINX_MAX_UPLOAD:-0}
 
 # Get the number of workers for Nginx, default to 1
 USE_NGINX_WORKER_PROCESSES=${NGINX_WORKER_PROCESSES:-1}
-# Modify the number of worker processes in Nginx config
-sed -i "/worker_processes\s/c\worker_processes ${USE_NGINX_WORKER_PROCESSES};" /etc/nginx/nginx.conf
 
-# Set the max number of connections per worker for Nginx, if requested
+# Set the max number of connections per worker for Nginx, if requested 
 # Cannot exceed worker_rlimit_nofile, see NGINX_WORKER_OPEN_FILES below
-if [ -n "$NGINX_WORKER_CONNECTIONS" ] ; then
-    sed -i "/worker_connections\s/c\    worker_connections ${NGINX_WORKER_CONNECTIONS};" /etc/nginx/nginx.conf
-fi
-
-# Set the max number of open file descriptors for Nginx workers, if requested
-if [ -n "$NGINX_WORKER_OPEN_FILES" ] ; then
-    echo "worker_rlimit_nofile ${NGINX_WORKER_OPEN_FILES};" >> /etc/nginx/nginx.conf
-fi
+NGINX_WORKER_CONNECTIONS=${NGINX_WORKER_CONNECTIONS:-1024}
 
 # Get the listen port for Nginx, default to 80
 USE_LISTEN_PORT=${LISTEN_PORT:-80}
-# Modify Nignx config for listen port
-if ! grep -q "listen ${USE_LISTEN_PORT};" /etc/nginx/conf.d/nginx.conf ; then
-    sed -i -e "/server {/a\    listen ${USE_LISTEN_PORT};" /etc/nginx/conf.d/nginx.conf
+
+content='user  nginx;\n'
+# Set the number of worker processes in Nginx
+content=$content"worker_processes ${USE_NGINX_WORKER_PROCESSES};\n"
+content=$content'error_log  /var/log/nginx/error.log warn;\n'
+content=$content'pid        /var/run/nginx.pid;\n'
+content=$content'events {\n'
+content=$content"    worker_connections ${NGINX_WORKER_CONNECTIONS};\n"
+content=$content'}\n'
+content=$content'http {\n'
+content=$content'    include       /etc/nginx/mime.types;\n'
+content=$content'    default_type  application/octet-stream;\n'
+content=$content'    log_format  main  '"'\$remote_addr - \$remote_user [\$time_local] \"\$request\" '\n"
+content=$content'                      '"'\$status \$body_bytes_sent \"\$http_referer\" '\n"
+content=$content'                      '"'\"\$http_user_agent\" \"\$http_x_forwarded_for\"';\n"
+content=$content'    access_log  /var/log/nginx/access.log  main;\n'
+content=$content'    sendfile        on;\n'
+content=$content'    keepalive_timeout  65;\n'
+# Generate Nginx config for maximum upload file size
+content=$content'    server {\n'
+content=$content"        listen ${USE_LISTEN_PORT};\n"
+content=$content'        location / {\n'
+content=$content'            include uwsgi_params;\n'
+content=$content'            uwsgi_pass unix:///tmp/uwsgi.sock;\n'
+content=$content'        }\n'
+content=$content'    }\n'
+content=$content"    client_max_body_size $USE_NGINX_MAX_UPLOAD;\n"
+content=$content'    include /etc/nginx/conf.d/*.conf;\n'
+content=$content'}\n'
+content=$content'daemon off;\n'
+# Set the max number of open file descriptors for Nginx workers, if requested
+if [ -n "${NGINX_WORKER_OPEN_FILES}" ] ; then
+    content=$content"worker_rlimit_nofile ${NGINX_WORKER_OPEN_FILES};\n"
 fi
+
+# Save generated nginx.conf
+printf "$content" > /etc/nginx/nginx.conf
+
 exec "$@"
