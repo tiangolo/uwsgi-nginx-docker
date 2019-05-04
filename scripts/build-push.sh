de5b32fb3d40f51cc07aa22c812316552cfc735e
Sebastián Ramírez <tiangolo@gmail.com>
2019-05-04 20:03:25 +0400

    :bug: Fix deploy build and push for each image

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker-compose -f docker-compose.build.yml build
+docker-compose -f docker-compose.build.yml build $NAME
 
-docker-compose -f docker-compose.build.yml push
+docker-compose -f docker-compose.build.yml push $NAME
